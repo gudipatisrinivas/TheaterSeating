a6de2741cca91270d19961170d5de7a4b149ba8a
Srinivas Gudipati <srinivasgudipati321@gmail.com>
2018-01-21 11:29:01 -0500

    Theater Layout

--- src/com/theaterseating/services/TheaterSeatingService.java
+++ src/com/theaterseating/services/TheaterSeatingService.java
@@ -1,16 +1,21 @@
 package com.theaterseating.services;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
-import com.theaterseating.BO.TheaterLayout;
-import com.theaterseating.BO.TheaterSeatingRequest;
+import com.theaterseating.bo.TheaterLayout;
+import com.theaterseating.bo.TheaterSeatingRequest;
 
 public interface TheaterSeatingService {
     
-    TheaterLayout getTheaterLayout(String rawLayout);
+    TheaterLayout getTheaterLayout(Map<Integer, LinkedList<Integer>> layOut);
     
-    List<TheaterSeatingRequest> getTicketRequests(String ticketRequests);
+    List<TheaterSeatingRequest> getTicketRequests(Map<Integer, LinkedHashMap<String, Integer>> ticketRequest);
     
     void processTicketRequests(TheaterLayout layout, List<TheaterSeatingRequest> requests);
+
+	void processAllTicketRequests(Map<Integer, LinkedList<Integer>> theaterMap,Map<Integer, LinkedHashMap<String, Integer>> requestName);
 
 }
